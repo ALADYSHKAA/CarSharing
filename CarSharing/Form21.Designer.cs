9ad1635b7aa4b4c4dada2856a3e54e75407ba826
nomad0192 <nomad0192@gmail.com>
2021-05-14 03:08:32 +0300

    commiy

--- CarSharing/Form21.Designer.cs
+++ CarSharing/Form21.Designer.cs
@@ -260,7 +260,7 @@ namespace CarSharing
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(13, 487);
+            this.button1.Location = new System.Drawing.Point(12, 487);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(86, 27);
             this.button1.TabIndex = 16;
